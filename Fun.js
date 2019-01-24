f0ef655a91750a32799ebc4381c756f52c46df44
Kaleb Raymond <43358126+KalebRaymond@users.noreply.github.com>
2019-01-23 21:27:43 -0500

    Add files via upload

--- Fun.js
+++ Fun.js
@@ -3,8 +3,8 @@
     document.onmousemove = handleMouseMove;
     function handleMouseMove(event) {
 		document.title = "" + event.pageX + ", " + event.pageY; 
-		document.getElementsByClassName("chase_img")[0].style.borderColor = "white";
-		document.getElementsByClassName("chase_img")[0].style.top = (event.pageY) + "px";
-		document.getElementsByClassName("chase_img")[0].style.left = (event.pageX) + "px";
+		
+		document.getElementsByClassName("chase_img")[0].style.top = (event.pageY - 200) + "px";
+		document.getElementsByClassName("chase_img")[0].style.left = (event.pageX - 650) + "px";
     }
 })();
